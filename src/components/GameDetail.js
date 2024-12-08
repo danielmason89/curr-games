69941880c19c665136a939b0c1607f9e6490b9d4
Lucas Winkler <lucaswinkler@gmail.com>
2024-12-07 22:30:12 -0500

    refactor: install react-icons and refactor platform image logic
    - Installed react-icons to replace local image files with platform specific icons.
    - Added layout styling to the new platforms icon list to look similar to the previous image styling with images.
    - Replaced the getPlatforms switch statement with an object key-value map.

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -1,69 +1,77 @@
-import React from "react";
+import React from 'react';
 // Styling and Animation
-import styled from "styled-components";
-import { motion } from "framer-motion";
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 // Redux
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { smallImage } from "../util";
-// Images
-import playstation from "../img/playstation.svg";
-import nintendo from "../img/nintendo.svg";
-import steam from "../img/steam.svg";
-import xbox from "../img/xbox.svg";
-import apple from "../img/apple.svg";
-import gamepad from "../img/gamepad.svg";
-import xboxOne from "../img/4375141_logo_xbox_icon.png";
-import playstation5Icon from "../img/7048152_playstation_ps5_icon.png";
-import android from "../img/icons8-android-64.png";
-import nintendods from "../img/icons8-nintendo-ds-50.png";
-import nintendodWiiU from "../img/icons8-nintendo-wii-u-30.png";
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { smallImage } from '../util';
+// Icons
+import { FaXbox, FaApple, FaLinux, FaGamepad, FaWindows } from 'react-icons/fa';
+import {
+  SiNintendo,
+  SiPlaystation,
+  SiPlaystation2,
+  SiPlaystation3,
+  SiPlaystation4,
+  SiPlaystation5,
+  SiPlaystationportable,
+  SiPlaystationvita,
+} from 'react-icons/si';
+import { IoLogoAndroid } from 'react-icons/io';
 
 // Star Images
-import starFull from "../img/star-full.png";
-import starEmpty from "../img/star-empty.png";
+import starFull from '../img/star-full.png';
+import starEmpty from '../img/star-empty.png';
+
+// Maps Platforms to Icons
+const platformIconMap = {
+  pc: <FaWindows />,
+  linux: <FaLinux />,
+  playstation5: <SiPlaystation5 />,
+  playstation4: <SiPlaystation4 />,
+  playstation3: <SiPlaystation3 />,
+  playstation2: <SiPlaystation2 />,
+  playstation1: <SiPlaystation />,
+  'ps-vita': <SiPlaystationvita />,
+  psp: <SiPlaystationportable />,
+  'xbox-series-x': <FaXbox />,
+  'xbox-one': <FaXbox />,
+  xbox360: <FaXbox />,
+  'xbox-old': <FaXbox />,
+  'nintendo-switch': <SiNintendo />,
+  'nintendo-3ds': <SiNintendo />,
+  'nintendo-ds': <SiNintendo />,
+  'nintendo-wii-u': <SiNintendo />,
+  'wii-u': <SiNintendo />,
+  wii: <SiNintendo />,
+  gamecube: <SiNintendo />,
+  'nintendo-64': <SiNintendo />,
+  'game-boy': <SiNintendo />,
+  snes: <SiNintendo />,
+  nes: <SiNintendo />,
+  ios: <FaApple />,
+  macos: <FaApple />,
+  macintosh: <FaApple />,
+  'apple-ii': <FaApple />,
+  android: <IoLogoAndroid />,
+};
+
+const PlatformIcon = ({ platformName, platformSlug }) => {
+  // Gets the SVG icon with a fallback icon if the platform is not found in the map
+  const icon = platformIconMap[platformSlug] ?? <FaGamepad />;
+
+  return <li aria-label={`Platform: ${platformName}`}>{icon}</li>;
+};
 
 const GameDetail = ({ pathId }) => {
   const navigate = useNavigate();
   // Exit Detail
-  const exitDetailhandler = (e) => {
+  const exitDetailhandler = e => {
     const element = e.target;
-    if (element.classList.contains("shadow")) {
-      document.body.style.overflow = "auto";
-      navigate("/");
-    }
-  };
-
-  // Get Platform Images
-  const getPlatform = (platform) => {
-    switch (platform) {
-      case "PlayStation 4":
-        return playstation;
-      case "PlayStation 5":
-        return playstation5Icon;
-      case "Xbox One":
-        return xboxOne;
-      case "Xbox Series S/X":
-        return xbox;
-      case "PC":
-        return steam;
-      case "Nintendo Switch":
-        return nintendo;
-      case "Android":
-        return android;
-      case "iOS":
-      case "macOS":
-        return apple;
-      case "Web":
-        return gamepad;
-      case "Nintendo DS":
-      case "Nintendo 3DS":
-        return nintendods;
-      case "Wii U":
-      case "Wii":
-        return nintendodWiiU;
-      default:
-      // return gamepad;
+    if (element.classList.contains('shadow')) {
+      document.body.style.overflow = 'auto';
+      navigate('/');
     }
   };
 
@@ -74,20 +82,20 @@ const GameDetail = ({ pathId }) => {
 
     for (let i = 1; i <= 5; i++) {
       if (i <= rating) {
-        stars.push(<img alt="star" key={i} src={starFull} />);
+        stars.push(<img alt='star' key={i} src={starFull} />);
       } else {
-        stars.push(<img alt="star" key={i} src={starEmpty} />);
+        stars.push(<img alt='star' key={i} src={starEmpty} />);
       }
     }
     return stars;
   };
 
   // Data
-  const { screen, game, isLoading } = useSelector((state) => state.detail);
+  const { screen, game, isLoading } = useSelector(state => state.detail);
   return (
     <>
       {!isLoading && (
-        <CardShadow className="shadow" onClick={exitDetailhandler}>
+        <CardShadow className='shadow' onClick={exitDetailhandler}>
           <Detail LayoutId={pathId}>
             <Stats>
               <header>
@@ -97,21 +105,22 @@ const GameDetail = ({ pathId }) => {
                 <Stars>
                   <div>
                     <h3>Rating: {game.rating}</h3>
-                    <div className="star">{getStars()}</div>
+                    <div className='star'>{getStars()}</div>
                   </div>
                 </Stars>
                 <Platforms>
                   <div>
                     <h3>Platform</h3>
-                    {game.platforms &&
-                      game.platforms.map((data) => (
-                        <img
-                          key={data.platform.id}
-                          src={getPlatform(data.platform.name)}
-                          title={data.platform.name}
-                          alt={data.platform.name}
-                        ></img>
-                      ))}
+                    <ul>
+                      {game.platforms &&
+                        game.platforms.map(data => (
+                          <PlatformIcon
+                            key={data.platform.id}
+                            platformName={data.platform.name}
+                            platformSlug={data.platform.slug}
+                          />
+                        ))}
+                    </ul>
                   </div>
                 </Platforms>
               </Info>
@@ -126,9 +135,9 @@ const GameDetail = ({ pathId }) => {
             <Description>
               <p>{game.description_raw}</p>
             </Description>
-            <div className="gallery">
+            <div className='gallery'>
               {screen.results &&
-                screen.results.map((screen) => (
+                screen.results.map(screen => (
                   <img
                     src={smallImage(screen.image, 1280)}
                     key={screen.id}
@@ -243,6 +252,18 @@ const Platforms = styled(motion.div)`
     width: 3.75rem;
     margin: 1rem 0.75rem;
   }
+  ul {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    list-style: none;
+    flex-wrap: wrap;
+  }
+  ul > li > svg {
+    font-size: 3.75rem;
+    margin: 1rem 0.75rem;
+  }
   @media (max-width: 768px) {
     h3 {
       font-size: 0.9rem;
@@ -250,6 +271,9 @@ const Platforms = styled(motion.div)`
     img {
       width: 3.25rem;
       margin: 1rem 0.75rem;
+    }
+    ul > li > svg {
+      font-size: 3.25rem;
     }
     padding-left: 0rem;
   }
