f13eb8b084ed2e8ba52787b9f59991f37a514e22
Lucas Winkler <lucaswinkler@gmail.com>
2025-03-09 22:30:29 -0400

    feat: implement nsfw filter
    - Removes games marked as nsfw by checking their tags and title for keywords

---
+++ src/server/utils/nsfwFilter.ts
@@ -0,0 +1,21 @@
+import type { GamesResponse } from '../lib/schemas.js';
+
+const nsfwTags = ['nsfw', 'erotic', 'hentai', 'sexual content'];
+const nsfwKeywords = ['sex', 'adult', 'erotic', 'hentai', 'sexual', 'nsfw'];
+
+export const filterNsfwGames = (res: GamesResponse): GamesResponse => {
+  return {
+    ...res,
+    results: res.results.filter(game => {
+      const hasNsfwTag = game.tags?.some(tag =>
+        nsfwTags.includes(tag.name.toLowerCase())
+      );
+
+      const hasNsfwTitle = nsfwKeywords.some(keyword =>
+        game.name?.toLowerCase().includes(keyword)
+      );
+
+      return !(hasNsfwTag || hasNsfwTitle);
+    }),
+  };
+};
