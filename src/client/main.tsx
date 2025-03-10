ae6de1a9a650b4c98c277277e9e2d37f03ef8cb4
Lucas Winkler <lucaswinkler@gmail.com>
2025-03-09 23:24:39 -0400

    feat: add placeholder game details page

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -4,15 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import SearchResults from '@/client/pages/SearchResults';
-
 import RootLayout from './layouts/RootLayout';
 import Home from './pages/Home';
-// import GameDetails from './pages/GameDetails';
 import NotFound from './pages/NotFound';
-
-// Styles and Animation
 import GlobalStyles from './components/GlobalStyles';
 import reportWebVitals from './reportWebVitals';
+import GameDetails from './pages/GameDetails';
 
 // Wrap the app with Redux Provider to enable RTK Query throughout the app
 // This is simpler than traditional Redux as we don't need to manage actions/reducers
@@ -24,8 +21,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Routes>
           <Route element={<RootLayout />}>
             <Route index element={<Home />} />
-            {/* <Route path='games/:id' element={<GameDetails />} /> */}
             <Route path='search' element={<SearchResults />} />
+            <Route path='games/:id' element={<GameDetails />} />
             <Route path='*' element={<NotFound />} />
           </Route>
         </Routes>
